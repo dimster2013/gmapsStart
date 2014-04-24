74d77c4389ab388e77dde82658c949faed096843
dimster2013 <dingen2010@gmail.com>
2014-04-24 15:47:52 +1000

    dfsf

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.controller('OtherAddressesController', function($scope) {
 
 app.controller('TabsDemoCtrl',function ($scope) {
     $scope.tabs = [
-        { title:"Dynamic Title 1", content:"Dynamic content 1" },
-        { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
+        { title:"Office", content:'office.html' },
+        { title:"Clinic", content:'clinic.html', disabled: true }
     ];
 })
 
