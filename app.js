6e7093810f1f29620d6ac14b0bbefef9170a7c5b
dimster2013 <dingen2010@gmail.com>
2014-04-24 15:15:02 +1000

    dfsf

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 /**
  * Created by Dimitri on 24/04/2014.
  */
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.bootstrap']);
 
-app.controller('MapsController', function($scope) {
-
+app.controller('OtherAddressesController', function($scope) {
 });
 
-app.directive('maps',function(){
+app.controller('TabsDemoCtrl',function ($scope) {
+    $scope.tabs = [
+        { title:"Dynamic Title 1", content:"Dynamic content 1" },
+        { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
+    ];
+})
+
+app.directive('otheraddresses',function(){
     return {
         restrict: 'E',
         replace: true,
         templateUrl: 'maps.html'
     };
 })
+
+
