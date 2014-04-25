f1969b8ab3794d8c9c94fb774128c28512ed73c0
dimster2013 <dingen2010@gmail.com>
2014-04-25 15:56:49 +1000

    dfsf

--- organisation/controllers/tabsController.js
+++ organisation/controllers/tabsController.js
@@ -3,7 +3,7 @@
  */
 app.controller('tabsController',function ($scope) {
     $scope.tabs = [
-        { title:"Correspondance", content:'organisation/views/correspondance.html' },
+        { title:"Correspondance", content:'organisation/views/correspondence.html' },
         { title:"Office", content:'organisation/views/office.html' },
         { title:"Clinic", content:'organisation/views/clinic.html' }
     ];
