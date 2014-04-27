1c86a3bbc4ba07a2a0cc9743eb3fb3e849d40c6c
dimster2013 <dingen2010@gmail.com>
2014-04-27 20:46:35 +1000

    dfsf

--- organisation/controllers/mapsController.js
+++ organisation/controllers/mapsController.js
@@ -1,70 +1,99 @@
 //This is worth to read for async processing
 //http://docs.angularjs.org/api/ng.$q
 //You need to use $q, and $rootScope.$apply
-app.service('GeoCoder', function ($q, $rootScope) {
-    return {
-        getLocations: function (address) {
-            var deferred = $q.defer();
-            var geocoder = new google.maps.Geocoder();
-            console.log('address', address);
-            geocoder.geocode({'address': address }, function (results, status) {
-                $rootScope.$apply(function () {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        console.log(results);
-                        deferred.resolve(results);
-                    }
-                });
-            });
-            return deferred.promise;
-        }
-    }
+//app.service('GeoCoder', function ($q, $rootScope) {
+//    return {
+//        getLocations: function (address) {
+//            var deferred = $q.defer();
+//            var geocoder = new google.maps.Geocoder();
+//            console.log('address', address);
+//            geocoder.geocode({'address': address }, function (results, status) {
+//                $rootScope.$apply(function () {
+//                    if (status == google.maps.GeocoderStatus.OK) {
+//                        console.log(results);
+//                        deferred.resolve(results);
+//                    }
+//                });
+//            });
+//            return deferred.promise;
+//        }
+//    }
+//});
+
+
+"use strict";
+
+app.controller('MapCtrl', function ($scope) {
+    $scope.maps = [
+        { center: {lat: 43.6, lng: 4}, zoom: 10 },
+        { center: {lat: 42, lng: 9}, zoom: 8 }
+    ];
 });
+//$scope.address = $scope.streetNumber + "," + $scope.streetName + "," + $scope.city+","+$scope.country;
 
-app.controller('MapCtrl', function ($scope, GeoCoder) {
-    $scope.map; // this will be set by directive
-    $scope.address = $scope.streetNumber + "," + $scope.streetName + "," + $scope.city+","+$scope.country;
+//    $scope.addMarkerFromAddress = function () {
+//
+//        if ($scope.position!=null) {
+//
+//        }
+//        else {
+//
+//            GeoCoder.getLocations($scope.address).then(function (results) {
+//                var latLng = results[0].geometry.location;
+//                var marker = new google.maps.Marker({
+//                    map: $scope.map,
+//                    position: latLng
+//                });
+//                $scope.map.setCenter(latLng);
+//            });
+//        }
+//    };
 
-    $scope.addMarkerFromAddress = function () {
 
-        if ($scope.position!=null) {
-            var latLng =$scope.position;
-
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                center:  new google.maps.LatLng(latLng.A, latLng.k)
-            });
-            $scope.map.setCenter(new google.maps.LatLng(parseFloat(latLng.A)
-                , parseFloat(latLng.k)));
-
-
-        }
-        else {
-
-            GeoCoder.getLocations($scope.address).then(function (results) {
-                var latLng = results[0].geometry.location;
-                var marker = new google.maps.Marker({
-                    map: $scope.map,
-                    position: latLng
-                });
-                $scope.map.setCenter(latLng);
-            });
-        }
-    };
-
-});
-
-app.directive('map', function () {
+app.directive('gmaps', function factory($timeout) {
     return {
-        restrict: 'E',
+        restrict: 'EA',
+        template: '<div class="gmaps"></div>',
         replace: true,
-        template: '<div></div>',
-        link: function (scope, element, attrs) {
-            var map_options = {
-                center: new google.maps.LatLng(40.294163, -3.7581804000000147),
-                zoom: 8
+        scope: {
+            center: '=center',
+            zoom: '=zoom'
+        },
+        link: function postLink(scope, iElement, iAttrs) {
+
+            var mapOptions = {
+                zoom: scope.zoom,
+                center: new google.maps.LatLng(scope.center.lat, scope.center.lng),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
             };
-            var map = new google.maps.Map(element[0], map_options);
-            scope.map = map;
+            var map = new google.maps.Map(iElement[0], mapOptions);
+
+            scope.$watch('center', function () {
+                map.setCenter(new google.maps.LatLng(parseFloat(scope.center.lat)
+                    , parseFloat(scope.center.lng)));
+            }, true);
+
+            google.maps.event.addListener(map, 'center_changed', function () {
+                $timeout(function () {
+                    var center = map.getCenter();
+                    scope.center.lat = center.lat();
+                    scope.center.lng = center.lng();
+                });
+            });
+
+            scope.$watch('zoom', function () {
+                map.setZoom(parseInt(scope.zoom));
+            });
+
+            google.maps.event.addListener(map, 'zoom_changed', function () {
+                $timeout(function () {
+                    scope.zoom = map.getZoom();
+                });
+            });
+
         }
     };
-})
+});
+
+
+
