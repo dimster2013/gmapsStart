b58b546961a2d56a51b06b6b1ea34455e72beb3b
dimster2013 <dingen2010@gmail.com>
2014-04-28 21:00:04 +1000

    dfsf

--- organisation/controllers/mapsController.js
+++ organisation/controllers/mapsController.js
@@ -1,6 +1,3 @@
-//This is worth to read for async processing
-//http://docs.angularjs.org/api/ng.$q
-
 
 //get address geocoded
 app.service('GeoCoder', function ($q, $rootScope) {
@@ -23,48 +20,8 @@ app.service('GeoCoder', function ($q, $rootScope) {
     }
 });
 
-//get orgdetails organisation (lat/long) amongst them
-//if latlong is available use this
-//otherwise geocode address if available
-app.service('Organisation', function ($q, $rootScope,mockdataService) {
-    return {
-        getOrgdetails: function (request) {
-            var deferred = $q.defer();
 
-            var mockData=true;
-
-            if (mockData) {
-                mockdataService.getOrganisation(request, function (results) {
-                    $rootScope.$apply(function () {
-                        console.log('hoera',results);
-
-                        $scope.streetNumber = results[0].Address.streetNumber;
-                        $scope.streetName = results[0].Address.streetName;
-                        $scope.city = results[0].Address.city;
-                        $scope.position = results[0].Address.position;
-
-                        deferred.resolve(results);
-                    });
-                });
-
-                return deferred.promise;
-            }
-            else
-            {
-                //call real service
-                var test=0;
-            }
-        }
-    }
-});
-
-
-app.controller('MapCtrl', function ($scope,Organisation) {
-
-//    $scope.center =
-//    {lat: 44, lng: 3};
-    $scope.zoom = 10;
-
+app.controller('MapCtrl', function ($scope, mockdataService) {
 
     var request = {
         "@schemaLocation": "http://nhsd.com.au/v1/OrganisationFinder/GetOrganisationRequest GetOrganisationRequest.xsd",
@@ -85,160 +42,47 @@ app.controller('MapCtrl', function ($scope,Organisation) {
         }
     }
 
-    var results;
-    //call orgservice to retreive org details
-   Organisation.getOrgdetails(request);
-
+    $scope.request = request;
 
 });
 
-//    var response = mockdataService.getOrganisation(request);
-//
+
+app.directive('gmaps', function factory($timeout, $q, GeoCoder,mockdataService) {
+    return {
+        restrict: 'EA',
+        template: '<div class="gmaps"></div>',
+        replace: true,
+
+        link: function postLink(scope, iElement, iAttrs) {
+
+            //call  to return map details and setcenter to this location
+
+            var centerPromise = mockdataService.getOrganisation(scope.request).then(function (results) {
+                console.log('hoerb', results);
+
+                var lng = results.OrganisationBom.Contact["0"].Address.lng;
+                var lat = results.OrganisationBom.Contact["0"].Address.lat;
 
 
+                //todo insert another promise for geocoding here
+                var mapOptions = {
+                    zoom: 10,
+                    center: new google.maps.LatLng(lng, lat),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                var map = new google.maps.Map(iElement[0], mapOptions);
+
+                map.setCenter(new google.maps.LatLng(parseFloat(lng), parseFloat(lat)));
+
+                return new $q.defer().promise;
+
+            });
 
 
-
-//
-//    if (mockdataService.position != null) {
-//
-//    }
-//    else {
-//
-//    }
-//
-//    var servicePromise = GeoCoder.getLocations(scope.address).then(function (results) {
-//
-//
-//        var latLng = results[0].geometry.location;
-
-//            console.log(latLng.A);
-//            console.log(latLng.k);
-//
-//            var mapOptions = {
-//                zoom: 10,
-//                center: new google.maps.LatLng(latLng.A, latLng.k),
-//                mapTypeId: google.maps.MapTypeId.ROADMAP
-//            };
-//
-//            var map = new google.maps.Map(iElement[0], mapOptions);
-//
-//            map.setCenter(new google.maps.LatLng(parseFloat(latLng.k), parseFloat(latLng.A)));
-
-    //return new $q.defer().promise;
-
-
-//if (1 == 0) {
-//    $scope.center =
-//    {lat: 44, lng: 3};
-//    $scope.zoom = 10;
-//}
-//else {
-//    $scope.streetNumber = "12";
-//    //$scope.streetName = "paris street";
-//    $scope.city = "paris";
-//    $scope.country = "france";
-//
-//    $scope.address = $scope.streetNumber + "," + $scope.streetName + "," + $scope.city + "," + $scope.country;
-//}
-//        GeoCoder.getLocations($scope.address).then(function (results) {
-//            var latLng = results[0].geometry.location;
-//            $scope.center =
-//            {lat: latLng.k, lng: latLng.A};
-//            $scope.zoom = 10;
-//        });
-
-
-
-
-
-
-
-//    $scope.addMarkerFromAddress = function () {
-//
-//        if ($scope.position!=null) {
-//
-//        }
-//        else {
-//
-//            GeoCoder.getLocations($scope.address).then(function (results) {
-//                var latLng = results[0].geometry.location;
-//                var marker = new google.maps.Marker({
-//                    map: $scope.map,
-//                    position: latLng
-//                });
-//                $scope.map.setCenter(latLng);
-//            });
-//        }
-//    };
-
-//
-
-//
-//app.directive('gmaps', function factory($timeout, $q, GeoCoder,Mapper) {
-//    return {
-//        restrict: 'EA',
-//        template: '<div class="gmaps"></div>',
-//        replace: true,
-//
-//        link: function postLink(scope, iElement, iAttrs) {
-//
-////call Mapper.GetMapLocation to return map details and setcenter to this location
-//            var centerPromise = Mapper.GetMapLocation(scope.address).then(function (results) {
-//                var latLng = results[0].geometry.location;
-//
-//                console.log(latLng.A);
-//                console.log(latLng.k);
-//
-//                var mapOptions = {
-//                    zoom: 10,
-//                    center: new google.maps.LatLng(latLng.A, latLng.k),
-//                    mapTypeId: google.maps.MapTypeId.ROADMAP
-//                };
-//
-//                var map = new google.maps.Map(iElement[0], mapOptions);
-//
-//                map.setCenter(new google.maps.LatLng(parseFloat(latLng.k), parseFloat(latLng.A)));
-//
-//                return new $q.defer().promise;
-//            });
-////            var mapOptions = {
-////                zoom: scope.zoom,
-////                center: new google.maps.LatLng(scope.center.lat, scope.center.lng),
-////                mapTypeId: google.maps.MapTypeId.ROADMAP
-////            };
-////            var map = new google.maps.Map(iElement[0], mapOptions);
-//
-////            scope.$watch('center', function () {
-////
-////
-////                map.setCenter(new google.maps.LatLng(parseFloat(scope.center.lat)
-////                    , parseFloat(scope.center.lng)));
-////
-////
-////            }, true);
-//
-////            google.maps.event.addListener(map, 'center_changed', function () {
-////                $timeout(function () {
-////                    var center = map.getCenter();
-////                    scope.center.lat = center.lat();
-////                    scope.center.lng = center.lng();
-////                });
-////            });
-////
-////            scope.$watch('zoom', function () {
-////                map.setZoom(parseInt(scope.zoom));
-////            });
-////
-////            google.maps.event.addListener(map, 'zoom_changed', function () {
-////                $timeout(function () {
-////                    scope.zoom = map.getZoom();
-////                });
-////            });
-//
-//        }
-//    };
-//});
+        }
+    };
+});
 
 
 
---
+++ organisation/controllers/mapscontroller_draft.js
@@ -0,0 +1,250 @@
+/**
+ * Created by Dimitri on 28/04/2014.
+ */
+//This is worth to read for async processing
+//http://docs.angularjs.org/api/ng.$q
+
+
+//get address geocoded
+app.service('GeoCoder', function ($q, $rootScope) {
+    return {
+        getLocations: function (address) {
+            var deferred = $q.defer();
+
+            var geocoder = new google.maps.Geocoder();
+            console.log('address', address);
+            geocoder.geocode({'address': address }, function (results, status) {
+                $rootScope.$apply(function () {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        console.log(results);
+                        deferred.resolve(results);
+                    }
+                });
+            });
+            return deferred.promise;
+        }
+    }
+});
+
+//get orgdetails organisation (lat/long) amongst them
+//if latlong is available use this
+//otherwise geocode address if available
+//app.service('Organisation', function ($q, $rootScope, mockdataService) {
+//    return {
+//        getOrgdetails: function (request) {
+//            var deferred = $q.defer();
+//
+//            var mockData = true;
+//
+//            if (mockData) {
+//                mockdataService.getOrganisation(request, function (results) {
+//
+//                    console.log('hoera', results);
+//
+//                    $scope.streetNumber = results[0].Address.streetNumber;
+//                    $scope.streetName = results[0].Address.streetName;
+//                    $scope.city = results[0].Address.city;
+//                    $scope.position = results[0].Address.position;
+//
+//                    deferred.resolve(results);
+//                });
+//
+//
+//                return deferred.promise;
+//            }
+//            else {
+//                //call real service
+//                var test = 0;
+//            }
+//        }
+//    }
+//});
+
+
+app.controller('MapCtrl', function ($scope, mockdataService) {
+
+//    $scope.center =
+//    {lat: 44, lng: 3};
+    $scope.zoom = 10;
+
+
+    var request = {
+        "@schemaLocation": "http://nhsd.com.au/v1/OrganisationFinder/GetOrganisationRequest GetOrganisationRequest.xsd",
+        "Identifier": {
+            "identifier_value": "String",
+            "identifier_schema": "http://www.altova.com/"
+        },
+        "Scope": {
+            "include_MutexMetadata": "true",
+            "include_RefMetadata": "true",
+            "mode": "EXCLUDE",
+            "Scope_Exception": [
+                "FACILITY",
+                [
+                    "KNOWN_AS"
+                ]
+            ]
+        }
+    }
+
+    $scope.request=request;
+    //call orgservice to retreive org details
+
+
+
+});
+
+//    var response = mockdataService.getOrganisation(request);
+//
+
+
+//
+//    if (mockdataService.position != null) {
+//
+//    }
+//    else {
+//
+//    }
+//
+//    var servicePromise = GeoCoder.getLocations(scope.address).then(function (results) {
+//
+//
+//        var latLng = results[0].geometry.location;
+
+//            console.log(latLng.A);
+//            console.log(latLng.k);
+//
+//            var mapOptions = {
+//                zoom: 10,
+//                center: new google.maps.LatLng(latLng.A, latLng.k),
+//                mapTypeId: google.maps.MapTypeId.ROADMAP
+//            };
+//
+//            var map = new google.maps.Map(iElement[0], mapOptions);
+//
+//            map.setCenter(new google.maps.LatLng(parseFloat(latLng.k), parseFloat(latLng.A)));
+
+//return new $q.defer().promise;
+
+
+//if (1 == 0) {
+//    $scope.center =
+//    {lat: 44, lng: 3};
+//    $scope.zoom = 10;
+//}
+//else {
+//    $scope.streetNumber = "12";
+//    //$scope.streetName = "paris street";
+//    $scope.city = "paris";
+//    $scope.country = "france";
+//
+//    $scope.address = $scope.streetNumber + "," + $scope.streetName + "," + $scope.city + "," + $scope.country;
+//}
+//        GeoCoder.getLocations($scope.address).then(function (results) {
+//            var latLng = results[0].geometry.location;
+//            $scope.center =
+//            {lat: latLng.k, lng: latLng.A};
+//            $scope.zoom = 10;
+//        });
+
+
+//    $scope.addMarkerFromAddress = function () {
+//
+//        if ($scope.position!=null) {
+//
+//        }
+//        else {
+//
+//            GeoCoder.getLocations($scope.address).then(function (results) {
+//                var latLng = results[0].geometry.location;
+//                var marker = new google.maps.Marker({
+//                    map: $scope.map,
+//                    position: latLng
+//                });
+//                $scope.map.setCenter(latLng);
+//            });
+//        }
+//    };
+
+//
+
+//
+app.directive('gmaps', function factory($timeout, $q, GeoCoder, Mapper) {
+    return {
+        restrict: 'EA',
+        template: '<div class="gmaps"></div>',
+        replace: true,
+
+        link: function postLink(scope, iElement, iAttrs) {
+
+//call Mapper.GetMapLocation to return map details and setcenter to this location
+
+            var centerPromise=  mockdataService.getOrganisation(scope.request).then(function (results) {
+                console.log('hoerb', results);
+                $scope.lng = results.OrganisationBom.Contact["0"].Address.lng;
+                $scope.lat = results.OrganisationBom.Contact["0"].Address.lat;
+
+                return new $q.defer().promise;
+
+            });
+
+//            var centerPromise = Mapper.GetMapLocation(scope.address).then(function (results) {
+//                var latLng = results[0].geometry.location;
+//
+//                console.log(latLng.A);
+//                console.log(latLng.k);
+//
+//                var mapOptions = {
+//                    zoom: 10,
+//                    center: new google.maps.LatLng(latLng.A, latLng.k),
+//                    mapTypeId: google.maps.MapTypeId.ROADMAP
+//                };
+//
+//                var map = new google.maps.Map(iElement[0], mapOptions);
+//
+//                map.setCenter(new google.maps.LatLng(parseFloat(latLng.k), parseFloat(latLng.A)));
+//
+//                return new $q.defer().promise;
+//            });
+
+
+//            var mapOptions = {
+//                zoom: scope.zoom,
+//                center: new google.maps.LatLng(scope.center.lat, scope.center.lng),
+//                mapTypeId: google.maps.MapTypeId.ROADMAP
+//            };
+//            var map = new google.maps.Map(iElement[0], mapOptions);
+
+//            scope.$watch('center', function () {
+//
+//
+//                map.setCenter(new google.maps.LatLng(parseFloat(scope.center.lat)
+//                    , parseFloat(scope.center.lng)));
+//
+//
+//            }, true);
+
+//            google.maps.event.addListener(map, 'center_changed', function () {
+//                $timeout(function () {
+//                    var center = map.getCenter();
+//                    scope.center.lat = center.lat();
+//                    scope.center.lng = center.lng();
+//                });
+//            });
+//
+//            scope.$watch('zoom', function () {
+//                map.setZoom(parseInt(scope.zoom));
+//            });
+//
+//            google.maps.event.addListener(map, 'zoom_changed', function () {
+//                $timeout(function () {
+//                    scope.zoom = map.getZoom();
+//                });
+//            });
+
+        }
+    };
+});
+
+
+
