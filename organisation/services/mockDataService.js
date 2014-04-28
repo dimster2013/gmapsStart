31a3fde0e2597c41aca07baa0e7c251326428b73
dimster2013 <dingen2010@gmail.com>
2014-04-28 16:28:16 +1000

    dfsf

--- organisation/services/mockDataService.js
+++ organisation/services/mockDataService.js
@@ -2,30 +2,14 @@
  * Created by Dimitri on 25/04/2014.
  */
 
-
 (function () {
-
     var mockdataFactory = function () {
-
         factory = {};
-
-
         factory.getOrganisation = function (request) {
-            return        {
-                "Address": {
-                    "streetNumber": 23,
-                    "streetName": "keizersgracht",
-                    "city": "amsterdam",
-                    "country":"the netherlands",
-                    "position": {"A": 15, "k": -33}
-                }
-            }
+            return $http.get("/mockdata/GetSmallOrgOtherAddresses.json");
         };
-
-
         return factory;
     };
-
 
     angular.module('app').factory('mockdataService', mockdataFactory);
 
