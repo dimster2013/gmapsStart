beb23ea359e3f5af2666b4275bdc9b656e5de013
dimster2013 <dingen2010@gmail.com>
2014-04-29 13:26:43 +1000

    dfsf

--- organisation/controllers/mapsController.js
+++ organisation/controllers/mapsController.js
@@ -17,7 +17,9 @@ app.controller('MapCtrl', function ($scope, mockdataService) {
             ]
         }
     }
+
     $scope.request = request;
+
 });
 
 app.directive('gmaps', function factory($timeout, $q, GeoCoder, mockdataService) {
@@ -30,52 +32,86 @@ app.directive('gmaps', function factory($timeout, $q, GeoCoder, mockdataService)
 
             var lat, lng;
             var centerPromise = mockdataService.getOrganisation(scope.request).then(function (results) {
-                console.log('org details:', results);
-                lat = results.OrganisationBom.Contact["0"].Address.lat;
-                lng = results.OrganisationBom.Contact["0"].Address.lng;
+                        console.log('org details:', results);
+                        lat = results.OrganisationBom.Contact["0"].Address.lat;
+                        lng = results.OrganisationBom.Contact["0"].Address.lng;
 
-                if (!lat | !lng) {
-                    var country = results.OrganisationBom.Contact["0"].Address.country["#text"];
-                    var suburb = results.OrganisationBom.Contact["0"].Address.suburb["#text"];
-                    scope.address = suburb + "," + country;
-                    if (suburb !=null) {
-                        var servicePromise = GeoCoder.getLocations(scope.address).then(function (results) {
-                            var latLng = results[0].geometry.location;
-                            lat = latLng.k;
-                            lng = latLng.A;
+                        if (!lat | !lng) {
+                            var country = results.OrganisationBom.Contact["0"].Address.country["#text"];
+                            var suburb = results.OrganisationBom.Contact["0"].Address.suburb["#text"];
+                            scope.address = suburb + "," + country;
+                            if (suburb != null) {
+                                var servicePromise = GeoCoder.getLocations(scope.address).then(function (results) {
+                                    var latLng = results[0].geometry.location;
+                                    lat = latLng.k;
+                                    lng = latLng.A;
 
-                            var mapOptions = {
-                                zoom: 10,
+                                    var mapOptions = {
+                                        zoom: 10,
+                                        center: new google.maps.LatLng(lat, lng),
+                                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                                    };
+
+                                    var map = new google.maps.Map(iElement[0], mapOptions);
+                                    scope.map = map;
+                                    console.log('lat', lat);
+                                    console.log('lng', lng);
+
+                                    //map.setCenter(new google.maps.LatLng(parseFloat(lat), parseFloat(lng)));
+
+                                    var map_options = {
+                                        center: new google.maps.LatLng(lat, lng),
+                                        zoom: 8
+                                    };
+                                    var map = new google.maps.Map(iElement[0], map_options);
+
+                                    var marker = new google.maps.Marker({
+                                        map: map,
+                                        position: latLng
+                                    });
+
+                                    map.setCenter(latLng);
+                                });
+
+                                return new $q.defer().promise;
+                            }
+                        }
+                        else {
+                            var latLng=new google.maps.LatLng(lat, lng);
+                            var map_options = {
                                 center: new google.maps.LatLng(lat, lng),
-                                mapTypeId: google.maps.MapTypeId.ROADMAP
+                                zoom: 8
                             };
+                            var map = new google.maps.Map(iElement[0], map_options);
 
-                            var map = new google.maps.Map(iElement[0], mapOptions);
-                            console.log('lat', lat);
-                            console.log('lng', lng);
-                            map.setCenter(new google.maps.LatLng(parseFloat(lat), parseFloat(lng)));
+                            var marker = new google.maps.Marker({
+                                map: map,
+                                position: latLng
+                            });
+
+                            map.setCenter(latLng);
+//                            var mapOptions = {
+//                                zoom: 10,
+//                                center: new google.maps.LatLng(lat, lng),
+//                                mapTypeId: google.maps.MapTypeId.ROADMAP
+//                            };
+//
+//                            var map = new google.maps.Map(iElement[0], mapOptions);
+//                            scope.map = map;
+//                            console.log('lat', lat);
+//                            console.log('lng', lng);
+//                            map.setCenter(new google.maps.LatLng(parseFloat(lat), parseFloat(lng)));
+
                             return new $q.defer().promise;
-                        });
+                        }
                     }
-                }
-                else {
-
-                    var mapOptions = {
-                        zoom: 10,
-                        center: new google.maps.LatLng(lat, lng),
-                        mapTypeId: google.maps.MapTypeId.ROADMAP
-                    };
-
-                    var map = new google.maps.Map(iElement[0], mapOptions);
-                    console.log('lat', lat);
-                    console.log('lng', lng);
-                    map.setCenter(new google.maps.LatLng(parseFloat(lat), parseFloat(lng)));
-                    return new $q.defer().promise;
-                }
-            });
+                )
+                ;
         }
-    };
-});
+    }
+        ;
+})
+;
 
 //get address geocoded
 app.service('GeoCoder', function ($q, $rootScope) {
